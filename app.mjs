13607a69d5ee770af4a84942487fc4a489b92d5b
Egor Kutilov <ekutia@gmail.com>
2022-12-17 09:24:28 +0100

    feat: add up and cd commands

--- app.mjs
+++ app.mjs
@@ -1,6 +1,9 @@
 import process from 'node:process';
 import readline from 'node:readline/promises';
 import os from 'node:os';
+import fs from 'node:fs/promises'
+import fsSync from 'node:fs'
+import path from 'node:path'
 
 let username = "";
 let curDir = os.homedir();
@@ -16,23 +19,36 @@ function parseArgs(){
     console.log(`Welcome to the File Manager, ${username}!`);
 }
 
-async function main(){
+function main(){
     const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
     rl.on("close", () => {
         console.log(`Thank you for using File Manager, ${username}, goodbye!`);
         rl.close();
     });
-    rl.on("line", (input) => 
-    {
-        if(input === ".exit")
-        {
-            rl.close();
+    rl.on("line", (input) => {
+        try {
+            if (input === ".exit") {
+                rl.close();
+            }
+            else if (input === "up") {
+                curDir = path.resolve(curDir, "../");
+            }
+            else if (input.startsWith("cd")) {
+                const args = input.split(" ");
+                const newCurDir = path.resolve(curDir, args[1]);
+                if(fsSync.existsSync(newCurDir)) {
+                    curDir = newCurDir;
+                }
+                else {
+                    throw new Error("Invalid input");
+                }
+            }
         }
-        else
-        {
-            console.log(`Your command is ${input}`);
-            console.log(`You are currently in ${curDir}`);
+        catch (err) {
+            console.log(err);
         }
+        console.log(`Your command is ${input}`);
+        console.log(`You are currently in ${curDir}`);
 
     });
     console.log(`You are currently in ${curDir}`);
