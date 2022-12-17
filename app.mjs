7c98bec9b4f9dbd651f9100e8137a6e74689dccf
Egor Kutilov <ekutia@gmail.com>
2022-12-17 14:34:08 +0100

    feat: add commands ls and cat, file launch.json

--- app.mjs
+++ app.mjs
@@ -1,9 +1,10 @@
 import process from 'node:process';
 import readline from 'node:readline/promises';
 import os from 'node:os';
-import fs from 'node:fs/promises'
+import * as StreamPromises from "stream/promises"
 import fsSync from 'node:fs'
 import path from 'node:path'
+import { once } from 'node:events';
 
 let username = "";
 let curDir = os.homedir();
@@ -25,10 +26,11 @@ function main(){
         console.log(`Thank you for using File Manager, ${username}, goodbye!`);
         rl.close();
     });
-    rl.on("line", (input) => {
+    rl.on("line", async (input) => {
         try {
             if (input === ".exit") {
                 rl.close();
+                return;
             }
             else if (input === "up") {
                 curDir = path.resolve(curDir, "../");
@@ -43,13 +45,35 @@ function main(){
                     throw new Error("Invalid input");
                 }
             }
+            else if (input == "ls") {
+                const content = fsSync.readdirSync(curDir, { withFileTypes: true });
+                let index = 0;
+                for(const dirEnt of content) {
+                    console.log(`${index}\t${dirEnt.name}\t\t${dirEnt.isDirectory() ? "directory" : "file"}`);
+                    index++;
+                }
+            }
+            else if(input.startsWith("cat")) {
+                const args = input.split(" ");
+                const filePath = path.resolve(curDir, args[1]);
+                if(fsSync.existsSync(filePath) && fsSync.lstatSync(filePath).isFile()) {
+                    const readableFromFile = fsSync.createReadStream(path.resolve(filePath));
+                    readableFromFile.pipe(process.stdout);
+                    await once(readableFromFile, 'end');
+                }
+                else {
+                    throw new Error("Invalid input");
+                }
+            }
+            else {
+                throw new Error("Invalid input");
+            }
         }
         catch (err) {
             console.log(err);
         }
         console.log(`Your command is ${input}`);
         console.log(`You are currently in ${curDir}`);
-
     });
     console.log(`You are currently in ${curDir}`);
 }
